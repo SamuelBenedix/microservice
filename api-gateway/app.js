2129cec6fed5e756fb0d1afc046c5edf4362111e
samuelbenedix <samuel1600.ss@gmail.com>
2021-03-24 23:40:21 +0700

    Create Product

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const courseRouter = require("./routes/courses");
+const mediaRouter = require("./routes/media");
+const paymentRouter = require("./routes/payment");
+const ordersRouter = require("./routes/orders");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,5 +22,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/courses", courseRouter);
+app.use("/media", mediaRouter);
+app.use("/payment", paymentRouter);
+app.use("/orders", ordersRouter);
 
 module.exports = app;
